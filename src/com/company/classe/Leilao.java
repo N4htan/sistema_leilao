2f0c050b873b365a8f7a7844569cabf6ebfa98af
n4htan <n4htan@hotmail.com>
2020-11-12 21:07:41 -0300

    primeiro

---
+++ src/com/company/classe/Leilao.java
@@ -0,0 +1,12 @@
+package com.company.classe;
+
+import java.util.Date;
+
+public class Leilao {
+    private Date data;
+    private String site;
+    private String endereco;
+    private String cidade;
+    private String estado;
+
+}
